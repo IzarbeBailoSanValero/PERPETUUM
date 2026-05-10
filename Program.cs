a54989a6f7995d57762138d7f0c4339224ea3b5d
IzarbeBailoSanValero <a28303@svalero.com>
2026-05-10 15:06:55 +0200

    disable audience validation in JWT authentication

--- Program.cs
+++ Program.cs
@@ -55,7 +55,7 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
        opt.TokenValidationParameters = new TokenValidationParameters
        {
            ValidateIssuer = true,
-           ValidateAudience = true,
+           ValidateAudience = false,
            ValidateLifetime = true,
            ValidateIssuerSigningKey = true,
            ValidIssuer = builder.Configuration["JWT:ValidIssuer"],
